2d8a53d6b9de06354de01b9a59c8a1239d2f7302
Arndís Einarsdóttir <arndis@Arndiss-MacBook-Pro.local>
2018-09-18 10:02:11 +0000

    Geymi kóða, byrja uppá nýtt

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,15 +1,13 @@
 
-
 stadsetning = 1.1
 if stadsetning == 1.1:
     print('You can travel: (N)orth')
-    direction = input('Direction: ')
-    while direction != 'N' or 'n':
+    direction = str(input('Direction: '))
+    while direction == 'S' or 's' or 'E' or 'e' or 'W' or 'w':
         print('Not valid direction')
-        direction = input('Direction: ')
-        if direction == 'N' or 'n':
+        direction = str(input('Direction: '))
+    else direction == 'N' or 'n':
             stadsetning = 1.1+0.1
-            break
 
 if stadsetning == 1.2:
     print('Tou can travel: (S)outh, (E)ast, (N)orth')
@@ -20,9 +18,21 @@ if stadsetning == 1.2:
     else:
         if direction == 'N' or 'n':
             stadsetning = 1.2+0.1
-        elif direction = == 'S' or 's':
+        elif direction == 'S' or 's':
             stadsetning = 1.2-0.1
-        else direction == 'E':
+        elif direction == 'E' or 'e':
             stadsetning = 1.2+1.0
-        break
+
+if stadsetning == 1.3:
+    print('Tou can travel: (S)outh, (E)ast')
+    direction = input('Direction: ')
+    while direction == 'W' or 'w' or 'N' or 'n':
+        print('Not valid direction')
+        direction = input('Direction: ')
+    else:
+        if direction == 'S' or 's':
+            stadsetning = 1.3-0.1
+        elif direction == 'E' or 'e':
+            stadsetning = 1.2-1.0
+    
 
