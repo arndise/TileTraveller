96f02896ebbe77c6797dd55c2c953f97cbef3f15
Arndís Einarsdóttir <arndis@Arndiss-MacBook-Pro.local>
2018-09-18 09:40:07 +0000

    Direction má vera stór og lítill stafur

--- TileTraveller.py
+++ TileTraveller.py
@@ -2,8 +2,27 @@
 
 stadsetning = 1.1
 if stadsetning == 1.1:
-    print('You can travel: (N)orth)')
+    print('You can travel: (N)orth')
     direction = input('Direction: ')
-    if direction != 'N':
+    while direction != 'N' or 'n':
         print('Not valid direction')
         direction = input('Direction: ')
+        if direction == 'N' or 'n':
+            stadsetning = 1.1+0.1
+            break
+
+if stadsetning == 1.2:
+    print('Tou can travel: (S)outh, (E)ast, (N)orth')
+    direction = input('Direction: ')
+    while direction == 'W' or 'w':
+        print('Not valid direction')
+        direction = input('Direction: ')
+    else:
+        if direction == 'N' or 'n':
+            stadsetning = 1.2+0.1
+        elif direction = == 'S' or 's':
+            stadsetning = 1.2-0.1
+        else direction == 'E':
+            stadsetning = 1.2+1.0
+        break
+
